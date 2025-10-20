3b42443b47dd214d83a2da4e53e7d6f53510a780
ksenya513 <karpickaamasya@gmail.com>
2025-10-21 00:32:11 +0700

    для вопроса

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main{
     public static void main(String[] args){
-        
+    }
+    public static Car[] getCarByBrand(Car[] cars,String brand){
+        int k=0;
+        for (Car car: cars){
+            if (car.getCarByBrand().equals(brand)){
+                k++;
+            }
+            
+        }
     }
 }
